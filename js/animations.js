1c10c758a32ec8f1fa1c501bc4cb86dcd2ece148
Braulio Lazo <yobraulio23@gmail.com>
2023-05-06 18:18:41 -0400

    CLEANING code

--- js/animations.js
+++ js/animations.js
@@ -1,8 +1,5 @@
 const animations = {
-    menuAnimations: () => {
-        const header = document.querySelector("#header");
-        const logoContainer = document.querySelector('.logo__container');
-        const elementsToDisableHover = document.querySelectorAll(".element-to-disable-hover");
+    menuAnimations: (header, logoContainer, elementsToDisableHover) => {
 
         if (scrollY > 0) {
             header.classList.add("blue__background");
@@ -20,52 +17,65 @@ const animations = {
         }
     },
 
-    closeMenuOnClickOutside: (event) => {
+    setupMenuToggle: () => {
         const menuButton = document.querySelector("#hamburger__menu");
         const menuLines = document.querySelectorAll(".hamburger__menu__line");
         const navegationContainer = document.querySelector(".navigation__links");
         const body = document.querySelector("body");
 
+        const topLine = menuLines[0];
+        const middleLine = menuLines[1];
+        const bottomLine = menuLines[2];
+
+        menuButton.addEventListener("click", () => {
+            animations.toggleMenu(topLine, middleLine, bottomLine, navegationContainer, body, menuButton);
+        });
+
+        document.addEventListener("click", (event) => {
+            animations.closeMenuOnClickOutside(event, topLine, middleLine, bottomLine, navegationContainer, body, menuButton);
+        });
+    },
+
+    closeMenuOnClickOutside: (event, topLine, middleLine, bottomLine, navegationContainer, body, menuButton) => {
         if (!navegationContainer.contains(event.target) && !menuButton.contains(event.target) && navegationContainer.classList.contains("menu__open")) {
-            menuLines[0].classList.remove("rotate__line__one");
-            menuLines[1].classList.remove("display__none");
-            menuLines[2].classList.remove("rotate__line__three");
+
+            topLine.classList.remove("rotate__line__one");
+            middleLine.classList.remove("display__none");
+            bottomLine.classList.remove("rotate__line__three");
+
             navegationContainer.classList.remove("menu__open");
+
             body.classList.remove("no__scroll");
         }
     },
 
-    toggleMenu: () => {
-        const menuButton = document.querySelector("#hamburger__menu");
-        const menuLines = document.querySelectorAll(".hamburger__menu__line");
-        const navegationContainer = document.querySelector(".navigation__links");
-        const body = document.querySelector("body");
+    toggleMenu: (topLine, middleLine, bottomLine, navegationContainer, body) => {
 
-        menuButton.addEventListener("click", () => {
-            menuLines[0].classList.toggle("rotate__line__one");
-            menuLines[1].classList.toggle("display__none");
-            menuLines[2].classList.toggle("rotate__line__three");
-            navegationContainer.classList.toggle("menu__open");
-            body.classList.toggle("no__scroll");
-        });
-
-        document.addEventListener("click", animations.closeMenuOnClickOutside);
+        topLine.classList.toggle("rotate__line__one");
+        middleLine.classList.toggle("display__none");
+        bottomLine.classList.toggle("rotate__line__three");
+        navegationContainer.classList.toggle("menu__open");
+        body.classList.toggle("no__scroll");
     },
 
     init: () => {
+        const header = document.querySelector("#header");
+        const logoContainer = document.querySelector('.logo__container');
+        const elementsToDisableHover = document.querySelectorAll(".element-to-disable-hover");
+
         if (window.innerWidth > 1024) {
-            window.onscroll = animations.menuAnimations;
+            window.onscroll = animations.menuAnimations(header, logoContainer, elementsToDisableHover);
         }
 
         window.addEventListener("resize", () => {
             if (window.innerWidth > 1024) {
-                window.onscroll = animations.menuAnimations;
+                window.onscroll = animations.menuAnimations(header, logoContainer, elementsToDisableHover);
             } else {
                 window.onscroll = null;
             }
         });
 
-        animations.toggleMenu();
+        animations.setupMenuToggle();
     }
 };
 
