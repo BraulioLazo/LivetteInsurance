2df89137f77dd95531a4703614b8bdd1e1e054d5
Braulio Lazo <yobraulio23@gmail.com>
2023-05-20 23:39:46 -0400

    Cambios en el FORMULARIO

--- js/formHandlers.js
+++ js/formHandlers.js
@@ -1,12 +1,5 @@
 
-//Esta funcion maneja el comportamiento en la entrada del numero de telefono.
-
-document.getElementById('user__phone__number').addEventListener('input', function (e) {
-    let x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
-    e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
-});
-
-//Formatea el numero del social security
+// Formatea el número del social security
 function formatSocialSecurityNumber(e) {
     let target = e.target, position = target.selectionStart, length = target.value.length;
     target.value = target.value.replace(/\D/g, '').replace(/(\d{3})(\d{2})(\d{4})/, '$1-$2-$3').substr(0, 11);
@@ -16,26 +9,16 @@ function formatSocialSecurityNumber(e) {
     }
 }
 
-window.onload = function () {
-    // Para el número de seguridad social del usuario
-    document.getElementById('user__social__security').addEventListener('input', function (e) {
-        formatSocialSecurityNumber(e);
-    });
 
-    // Para el número de seguridad social del cónyuge
-    document.getElementById('spouse__social__security').addEventListener('input', function (e) {
-        formatSocialSecurityNumber(e);
-    });
-};
-
+//Maneja la altura del FIELDSET
 function fixFieldsetHeight() {
     let valor = document.querySelector("#include_in_insurance").value.toLowerCase();
     const inputSpouse = document.querySelectorAll(".input__spouse");
 
-    if(valor == "si"){
+    if(valor == "si") {
         inputSpouse.forEach(element =>{
             element.setAttribute("required", "");
-        })
+        });
 
         if(window.innerWidth > 1200){
             document.querySelector("#fieldset__spouse").style.height = "250px";
@@ -48,25 +31,39 @@ function fixFieldsetHeight() {
         }
         document.querySelector("#fieldset__spouse").style.transition = "0.5s ease";
 
-    } else if(valor == "no"){
+    } else if(valor !== "si"){
         inputSpouse.forEach(element =>{
             element.removeAttribute("required");
-        })
+        });
+
         if(window.innerWidth > 768){
             document.querySelector("#fieldset__spouse").style.height = "150px";
         } else {
             document.querySelector("#fieldset__spouse").style.height = "230px";
-
         }
     }
 }
 
-window.onload = function() {
+
+window.addEventListener('load', function() {
+    // Esta función maneja el comportamiento en la entrada del número de teléfono.
+    document.getElementById('user__phone__number').addEventListener('input', function(e) {
+        let x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+        e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+    });
+
+    // Para el número de seguridad social del usuario
+    document.getElementById('user__social__security').addEventListener('input', function(e) {
+        formatSocialSecurityNumber(e);
+    });
+
+    // Para el número de seguridad social del cónyuge
+    document.getElementById('spouse__social__security').addEventListener('input', function(e) {
+        formatSocialSecurityNumber(e);
+    });
+
     document.querySelector("#include_in_insurance").addEventListener('change', fixFieldsetHeight);
     window.addEventListener('resize', fixFieldsetHeight);
-}
 
-
-
-
+});
 
