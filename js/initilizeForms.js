9e910ad3f4249932e7c709547eab126a9a3599ee
Braulio Lazo <yobraulio23@gmail.com>
2023-05-21 20:24:07 -0400

    Form BIG Change

---
+++ js/initilizeForms.js
@@ -0,0 +1,25 @@
+(function () {
+    function initialize() {
+
+        adjustSpouseFieldsetHeight();
+        // Esta función maneja el comportamiento en la entrada del número de teléfono.
+        document.getElementById('user__phone__number').addEventListener('input', function (e) {
+            let x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+            e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+        });
+
+        // Para el número de seguridad social del cónyuge
+        document.querySelectorAll('.input__social__security').forEach(element =>{
+            element.addEventListener('input', function (e) {
+                formatSocialSecurityNumber(e);
+            });
+        })
+
+
+        // Ajusta la altura del FIELDSET del cónyuge dependiendo del valor seleccionado
+        document.querySelector("#include_spouse_in_insurance").addEventListener('change', adjustSpouseFieldsetHeight);
+        window.addEventListener('resize', adjustSpouseFieldsetHeight);
+    }
+
+    window.addEventListener("load", initialize);
+})();
